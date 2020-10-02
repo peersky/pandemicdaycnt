57e28bcba20238c862af22a083b428825ea81176
Tim Pechersky <tim.pechersky@gmail.com>
2020-10-02 16:46:10 +0300

    Updates

---
+++ precache-manifest.862ea3ddbc06c3ba086d098d4ea9642c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ca06010a6db79bad33087428c0ae99",
+    "url": "/pandemicdaycnt/index.html"
+  },
+  {
+    "revision": "84170cb479464f1bec43",
+    "url": "/pandemicdaycnt/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30f83968da34945efc65",
+    "url": "/pandemicdaycnt/static/js/2.df992aaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pandemicdaycnt/static/js/2.df992aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84170cb479464f1bec43",
+    "url": "/pandemicdaycnt/static/js/main.d8c5279c.chunk.js"
+  },
+  {
+    "revision": "f5dc54042da6917a368e",
+    "url": "/pandemicdaycnt/static/js/runtime-main.660a3917.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pandemicdaycnt/static/media/logo.5d5d9eef.svg"
+  }
+]);
